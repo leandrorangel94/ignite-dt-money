4f5063e3f8ca807c1cd00d43f76ed7c5d1f09a85
Leandro Rangel <rangel94@outlook.com>
2023-06-06 19:11:30 -0300

    Conexão com a API - Configurando axios e buscando transações

--- src/pages/Transactions/SearchForm/index.tsx
+++ src/pages/Transactions/SearchForm/index.tsx
@@ -3,12 +3,16 @@ import { SearchFormContainer } from "./styles";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { TransactionsContext } from "../../../contexts/TransactionsContext";
+import { useContext } from "react";
 
 const searchFormSchema = z.object({ query: z.string() });
 
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
+  const { fetchTransactions } = useContext(TransactionsContext);
+
   const {
     register,
     handleSubmit,
@@ -18,8 +22,7 @@ export function SearchForm() {
   });
 
   async function handleSearchTransactions(data: SearchFormInputs) {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    console.log(data);
+    await fetchTransactions(data.query);
   }
 
   return (
